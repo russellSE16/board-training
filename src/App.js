3fde4b2cc9a283f9d7034fbb7eacabbc0276356b
Russell <67647336+russellSE16@users.noreply.github.com>
2021-01-20 16:49:46 +0000

    Tidy and comments for App.js and App.css

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.courseOptions = {
+      //Enter courses in this object with name to display as the key and string in Training.courses.name as value
       'Data Modelling' : 'data-modelling',
       'Analysis & Reporting' : 'analysis-reporting',
       'Planning & Procedures' : 'planning-procedures'
@@ -24,6 +25,7 @@ class App extends React.Component {
     this.resetExercise = this.resetExercise.bind(this);
   }
   loadCourseData(courseIndex) {
+    //When user selects a course, load the data into state, count how many exercises are marked complete and set the courseProgress in state
     const courseData = Training.courses[courseIndex];
     const completedExercises = courseData.exercises.filter(exercise => exercise.completed); 
     const progress = completedExercises.length;
@@ -33,6 +35,9 @@ class App extends React.Component {
     });
   }
   setCourse(course) {
+    //When user selects a course, find the index of that course in Training.courses array. 
+    //If course is loaded for the first time, set a completed property to each exercise and task equal to false.
+    //If course is loaded for the first time, set a current property on each exercise to the first task in the exercise.
     const courseIndex = Training.courses.findIndex(element => element.title === course);
     if (courseIndex === -1) {
       return;
@@ -57,6 +62,7 @@ class App extends React.Component {
     this.loadCourseData(courseIndex);
   }
   updateExerciseStatus(exerciseIndex, taskIndex, taskIsComplete, exerciseIsComplete) {
+    //Handles any change to the completion of tasks and exercises, updating the completed and current properties in Training module
     const courseIndex = Training.courses.findIndex(element => element.title === this.state.selectedCourse.title);
     Training.courses[courseIndex].exercises[exerciseIndex].tasks[taskIndex].completed = taskIsComplete;
     Training.courses[courseIndex].exercises[exerciseIndex].tasks[taskIndex].current = !taskIsComplete;
@@ -67,6 +73,7 @@ class App extends React.Component {
     this.loadCourseData(courseIndex);
   }
   resetExercise(exerciseIndex) {
+    //Handles the reset of an exercise, setting the completed properties of the exercise and all tasks to false and setting the current property to the first task
     const courseIndex = Training.courses.findIndex(element => element.title === this.state.selectedCourse.title);
     Training.courses[courseIndex].exercises[exerciseIndex].tasks.forEach((task, index) => {
       task.completed = false;
@@ -84,6 +91,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          {/* Only show this heading if a course hasn't been chosen yet */}
           {this.state.selectedCourse.title === undefined && <h1>Classroom Training Guide</h1>}
         </header>
         <CourseSelect
@@ -97,8 +105,9 @@ class App extends React.Component {
           <CSSTransitionGroup
             transitionName="fade"
             transitionEnterTimeout={500}
-            transitionLeaveTimeout={500}>                           
-              {this.state.selectedCourse.exercises.map((exercise, index) => {
+            transitionLeaveTimeout={500}>
+              {/* Render an Exercise component for each exercise in the selected course*/}
+              {this.state.selectedCourse.exercises.map((exercise, index) => { 
                 return (
                   <Exercise 
                     exercise={exercise}
