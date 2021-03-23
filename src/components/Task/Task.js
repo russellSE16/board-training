77ac020c41aae9cb7ffa7621cb5f2a37897bfe67
Russell <67647336+russellSE16@users.noreply.github.com>
2021-03-23 14:22:08 +0000

    Add info text and q&a input to Task component

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -5,10 +5,14 @@ class Task extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            hintOpen: false
+            hintOpen: false,
+            answer: 0,
+            answerCorrect: ''
         }
         this.handleClick = this.handleClick.bind(this);
         this.handleHintClick = this.handleHintClick.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleClick() {
         if (this.props.task.current) {
@@ -37,6 +41,35 @@ class Task extends React.Component {
             }
         }
     }
+    handleInput(event) {
+        this.setState({ answer: event.target.value });
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        const userAnswer = this.state.answer.toLowerCase();
+        const taskAnswer = this.props.answer.toLowerCase();
+        if (userAnswer == taskAnswer) {
+            this.setState({ answerCorrect: 'yes' });
+        } else {
+            this.setState({ answerCorrect: 'no' });
+        }
+
+    }
+    renderInput() {
+        if (!this.props.task.inputCaption) return null;
+
+        return (
+            <div className="task-input">
+                <form>
+                    <label htmlFor={this.props.index + 'answer'}>{this.props.task.inputCaption}</label>
+                    <input id={this.props.index + 'answer'} type="text" onChange={this.handleInput}></input>
+                    <input type="submit" value="Submit" onClick={this.handleSubmit}></input>
+                    {this.state.answerCorrect && <span>{this.state.answerCorrect === 'yes' ? 'Correct' : 'Incorrect'}</span>}
+                </form>
+            </div>
+        )
+        
+    }
     render() {
         const divClassName = () => {
             if (this.props.task.completed) {
@@ -61,6 +94,12 @@ class Task extends React.Component {
                     {this.props.task.image && <img src={`./img/${this.props.task.image}`} alt={this.props.task.imageAlt} />}
                 </div>
                 {this.renderHint()}
+                {this.renderInput()}
+                {this.props.task.infoJsx && (
+                    <div className="task-info">
+                        {this.props.task.infoJsx}
+                    </div>
+                )}
             </div>
         );
     }
