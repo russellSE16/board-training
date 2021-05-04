52e9f7ef45fcf366887e17bc39d69355469f2b62
Russell <67647336+russellSE16@users.noreply.github.com>
2021-05-04 23:39:48 +0100

    Refactor CourseSelect as functional component

--- src/components/CourseSelect/CourseSelect.js
+++ src/components/CourseSelect/CourseSelect.js
@@ -1,52 +1,37 @@
 import React from 'react';
 import './CourseSelect.css';
 
-class CourseSelect extends React.Component {
-    getOptionClass(option) {
-        if (this.props.selectedCourse === option) {
-            return 'active';
-        }
-        else {
-            return '';
-        }
+export default function CourseSelect ({ options, selectedCourse, setCourse, progress, max }) {
+    const handleCourseChange = course => {
+        setCourse(course);
     }
-    handleCourseChange(courseOption) {
-        this.props.setCourse(courseOption);
-    }
-    renderCourseOptions() {
-        return Object.keys(this.props.options).map(courseOption => {
-            const courseOptionValue = this.props.options[courseOption];
+    
+    const getCourseOptions = () => {
+        return Object.keys(options).map(courseOption => {
+            const courseOptionValue = options[courseOption];
             return (
                 <li 
                     key={courseOptionValue}
-                    className={this.getOptionClass(courseOptionValue)} 
-                    onClick={this.handleCourseChange.bind(this, courseOptionValue)}>
-                {courseOption}</li>
+                    className={selectedCourse === courseOptionValue ? 'active' : ''} 
+                    onClick={handleCourseChange.bind(this, courseOptionValue)}
+                >{courseOption}</li>
             );
         });
-    }
-    renderProgressBar() {
-        if (this.props.selectedCourse) {
-            return (
+    };
+
+    return (
+        <div className="CourseSelect">
+            <div className="CourseSelect-options">
+                <ul>
+                    {getCourseOptions()}    
+                </ul>
+            </div>
+            {selectedCourse && (
                 <div className="CourseSelect-progress">
                     <label htmlFor="progress">Course Completion:</label>
-                    <progress id="progress" value={this.props.progress} max={this.props.max}></progress>
+                    <progress id="progress" value={progress} max={max}></progress>
                 </div>
-            );
-        }
-    }
-    render() {
-        return (
-            <div className="CourseSelect">
-                <div className="CourseSelect-options">
-                    <ul>
-                        {this.renderCourseOptions()}    
-                    </ul>
-                </div>
-                {this.renderProgressBar()}
-            </div>
-        );
-    }
-}
-
-export default CourseSelect;
+            )}
+        </div>
+    );
+};
